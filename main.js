3610a3d3e86db8e1e65d2000571f989e4a7e9636
Kevin Chaves <kchaves@innovagency.com>
2024-04-10 14:30:11 +0100

    better log of state props

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import { logReactivity } from "./lib/logger.js";
+import { logState } from "./lib/logger.js";
 import State from "./lib/klacfw-state.js";
 
 const state = new State();
@@ -6,21 +6,21 @@ const state = new State();
 state.price = 5
 state.quantity = 5
 state.total = () => state.price * state.quantity
-state.totalWithTaxes = () => state.total() * 1.25
+state.totalWithTaxes = () => state.total() * state.price
 
-logReactivity(state);
+logState(state);
 
 state.price = 5
 state.quantity = 100
 
-logReactivity(state);
+logState(state);
 
 state.price = 1000
 state.quantity = 100
 
-logReactivity(state);
+logState(state);
 
 state.price = 1
 state.quantity = 1
 
-logReactivity(state);
+logState(state);
